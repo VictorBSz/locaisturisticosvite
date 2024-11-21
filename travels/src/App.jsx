da526c7c48370f415d2fe0a90c9e6ef3cd7ffb22
Victor Souza <victor.souza0350@gmail.com>
2024-11-20 23:33:09 -0300

    feat: added initial files using vite create

---
+++ travels/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppRoutes from "./routes/routes";
+
+export default function App() {
+  return (
+    <>
+	<div id= 'app page'>
+    <AppRoutes />
+  </div>
+    </>
+  );
+}
