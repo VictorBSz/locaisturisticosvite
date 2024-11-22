ff3dd7bea781b1beef78df372077667887bd848e
Victor Souza <victor.souza0350@gmail.com>
2024-11-21 22:29:26 -0300

    feat: added home test and berlin to routes

--- travels/src/routes/routes.jsx
+++ travels/src/routes/routes.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../pages/Home/index.jsx";
 import Sydney from "../pages/Sydney/index.jsx"
-
+import Berlin from '../pages/Berlin/index.jsx'
+import Test from "../pages/hometrue/index.jsx"
 
 export default function AppRoutes() {
     return(
@@ -10,6 +11,8 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sydney" element={<Sydney />} />
+                <Route path="/berlin" element={<Berlin />} />
+                <Route path="/test" element={<Test />} />
             </Routes>
         </BrowserRouter>
     );
